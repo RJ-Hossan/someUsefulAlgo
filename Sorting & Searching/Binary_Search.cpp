180e3ff3c04bfc0f861dbb083566c8f5f9069bc5
RJ_Hossan <61456789+RJ-Hossan@users.noreply.github.com>
2023-03-30 22:48:34 +0600

    Searching Algorithm

---
+++ Sorting & Searching/Binary_Search.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binary_search(int a[], int target, int low, int high){
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+        if(a[mid] == target)    return mid;
+        else if(a[mid] < target)   low = mid + 1;
+        else    high = mid - 1;
+    }
+    return -1;
+    // cout << "The target element fount at index no : " << index << endl;
+}
+
+int main(){
+    int t;  cin >> t;
+    while(t--){
+        int n;  cin >> n;
+        int a[n];
+        for(int i=0; i<n; i++)  cin >> a[i];
+        sort(a, a+n);
+        int x;  cin >> x;
+        if(binary_search(a, x, 0, n-1) == -1) cout << "The element isn't in the list.\n";
+        else{
+            cout << "The target element fount at index no : ";
+            cout << binary_search(a, x, 0, n-1) << endl;    // Indexing 0 base
+        }
+    }
+    return 0;
+}
+
+/*
+    Time Complexity: O(log n)
+    Auxiliary Space: O(1)
+*/
